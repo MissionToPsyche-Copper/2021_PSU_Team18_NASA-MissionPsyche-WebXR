0350b4cf1f6506fa633539cc8d64071d832c87b0
miablo <mvd5044@psu.edu>
2021-09-25 22:23:45 -0400

    Three.js Framework Testing && first commit/push - MD

---
+++ .idea/src/main.js
@@ -0,0 +1,9 @@
+// Three.js Framework Testing
+// Option 1: Import the entire three.js core library.
+import * as THREE from 'three';
+const scene = new THREE.Scene();
+
+
+// Option 2: Import just the parts you need.
+//import { Scene } from 'three';
+//const scene = new Scene();
